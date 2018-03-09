419c786c3843d19449303813cc3bafcf7b1d207c
Igor Vovk <ovodigr@gmail.com>
2018-03-09 09:58:38 +0200

    4

--- quick.c
+++ quick.c
@@ -1,22 +1,39 @@
 #include "wiring_private.h"
 #include "pins_arduino.h"
+
 void digitalWrite2(int pin, int on) {
 
   if (pin>7 && pin<=13)  
   {
-    	if (on==1)
-    	 PORTB = PORTB | (1<<(pin-8)); // устанавливаем состояние pin13 как HIGH //PORTB = PORTB || 0b10000000;
-    	else
-    	 PORTB = PORTB && (~1<<(pin-8)); // устанавливаем состояние pin13 как LOW  //(1<<8)//PORTB = PORTB && 0b01111111;
+		if (on==1)
+			PORTB = PORTB | (1<<(pin-8)); // устанавливаем состояние pin13 как HIGH 
+		else
+			PORTB = PORTB & ~(1<<(pin-8)); // устанавливаем состояние pin13 как LOW  
   }
   else
   
   if(pin>0 && pin<=7)
   {
-    	if (on)
-    	 PORTD = PORTD | (1<<pin); 
-    	else
-    	 PORTD = PORTD && (~1<<pin); 
-    }
-
+		if (on)
+			PORTD = PORTD | (1<<pin); 
+		else
+			PORTD = PORTD & ~(1<<pin); 
+  }
 }
+
+int digitalRead2(int pin) {
+
+	int status;
+  if (pin>7 && pin<=13)  
+			//status = PORTB >> (pin-8);  
+			status = PORTB && (1 << (pin-8));  
+  else
+  
+  if(pin>0 && pin<=7)
+			status = PORTD >> pin; 
+  
+  if (status==1) return HIGH;
+	return LOW;
+}
+
+
