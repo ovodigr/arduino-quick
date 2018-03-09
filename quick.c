590c6402971f13e85aa1e8ec952d2d99fbee77f4
Igor Vovk <ovodigr@gmail.com>
2018-03-09 20:57:05 +0200

    6

--- quick.c
+++ quick.c
@@ -3,37 +3,36 @@
 
 void digitalWrite2(int pin, int on) {
 
-  if (pin>7 && pin<=13)  
-  {
+if (pin>7 && pin<=13)  
+	{
 		if (on==1)
 			PORTB = PORTB | (1<<(pin-8)); // устанавливаем состояние pin13 как HIGH 
 		else
 			PORTB = PORTB & ~(1<<(pin-8)); // устанавливаем состояние pin13 как LOW  
-  }
-  else
-  
-  if(pin>0 && pin<=7)
-  {
+	}
+	else
+	if(pin>0 && pin<=7)
+	{
 		if (on)
 			PORTD = PORTD | (1<<pin); 
 		else
 			PORTD = PORTD & ~(1<<pin); 
-  }
+	}
 }
 
 int digitalRead2(int pin) {
 
-	int status=0;
+int status=0;
 
-  if (pin>7 && pin<=13)
-  	status = PORTB >> (pin-8);
-  else
-  if (pin>0 && pin<=7)
-  	status = PORTD >> pin;
- 	
-  
-   	 if (status>=1) return pin;
-	return pin;
+if (pin>7 && pin<=13)
+	status = PORTB >> (pin-8);
+else
+if (pin>0 && pin<=7)
+	status = PORTD >> 1;
+
+
+if (status>=1) return 1;
+return 1;
 }
 
 
